d45d7c4880c688a4838a18b7166920dcf7f69ed7
Francis Kojo Haizel <franciskhhaizel@gmail.com>
2026-03-11 01:37:06 +0000

    changes made in the server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,11 +25,10 @@ app.use("/api/messages", messageRoutes);
 if (ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("*", (_, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
-
 
 server.listen(PORT, () => {
     console.log("Server is running on port: " + PORT)
